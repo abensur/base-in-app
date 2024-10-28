fb684bc441b1236d5bd4995a2a52adb7a1571bcc
Rafael Abensur <rafael@abensur.me>
2024-10-28 12:04:59 -0300

    feat: add some deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-07-30',
-  // Nuxt 4 directory structure and features
-  // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
   future: { compatibilityVersion: 4 },
-  // Nuxt Modules
-  // https://nuxt.com/modules
+  
+  extends: ['@nuxt/ui-pro'],
   modules: [
     '@nuxthub/core',
-    '@nuxt/eslint'
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/fonts'
   ],
   hub: {
     database: true,
@@ -18,10 +18,8 @@ export default defineNuxtConfig({
   },
   nitro: {
     experimental: {
-      // Enable Server API documentation within NuxtHub
       openAPI: true
     }
   },
-  // Development
   devtools: { enabled: true },
 })
